fdba9f9170dbb108652d14b177eda3da9e04e9bc
gabriel <gabriel@tibas.london>
2017-10-22 22:24:05 +0100

    Cleanup.

--- rash/__main__.py
+++ rash/__main__.py
@@ -3,9 +3,23 @@ from curses import wrapper, beep
 from sys import stdout
 from time import sleep
 
+args = None
+
+
 def run(stdscr):
-    
+
     stdscr.clear()
+
+    for i in range(1000):
+        stdscr.addstr(0, 1, str(i) * 10)
+        a = stdscr.get_wch()
+        args.outfile.write(a)
+        stdscr.addstr(1, 1, a * 10)
+        beep()
+
+
+def main():
+    global args
 
     print('running module rash')
     parser = ArgumentParser()
@@ -14,24 +28,10 @@ def run(stdscr):
         type=FileType('w')
     )
     args = parser.parse_args()
-
-    for i in range(1000):
-        stdscr.addstr(0, 1, str(i) * 10)
-        a = stdscr.getkey()
-        args.outfile.write(a)
-        stdscr.addstr(1, 1, a * 10)
-        beep()
-
-
-
-
-def main():
-
-    wrapper(run)
-
-
-
-
+    try:
+        wrapper(run)
+    except KeyboardInterrupt:
+        print('ciao.')
 
 if __name__ == '__main__':
     main()
