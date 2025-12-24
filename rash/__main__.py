8b3c1a55143f602a5e876fc0b48483513f7057c6
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2025-12-24 03:49:21 +0100

    add quiet and centered

--- rash/__main__.py
+++ rash/__main__.py
@@ -8,12 +8,24 @@ import logging
 args = None
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-click_sound_path = os.path.join(script_dir, 'click.wav')
-end_sound_path = os.path.join(script_dir, 'end.wav')
+click_sound_path = os.path.join(script_dir, "click.wav")
+end_sound_path = os.path.join(script_dir, "end.wav")
+
 
 def ts():
     now = datetime.now()
     return now.strftime("\n\n[[Щ %Y-%m-%d (%A)/%H:%M]]::\n\n")
+
+
+def get_positions(stdscr, center):
+    if center:
+        height, width = stdscr.getmaxyx()
+        char_y, char_x = height // 2, width // 2
+        prompt_y, prompt_x = char_y + 2, char_x
+    else:
+        char_y, char_x = 1, 1
+        prompt_y, prompt_x = 3, 1
+    return char_y, char_x, prompt_y, prompt_x
 
 
 def run(stdscr):
@@ -23,34 +35,41 @@ def run(stdscr):
     time_limit = args.minutes * 60 if args.minutes else None
     char_limit = args.length
 
+    char_y, char_x, prompt_y, prompt_x = get_positions(stdscr, args.center)
+
     if args.prompt:
-        stdscr.addstr(3, 1, args.prompt)
+        stdscr.addstr(prompt_y, prompt_x, args.prompt)
 
     args.outfile.write(ts())
 
-    playsound(click_sound_path, block=False)
+    if not args.quiet:
+        playsound(click_sound_path, block=False)
 
     char_count = 0
     while True:
         a = stdscr.get_wch()
 
         if not isinstance(a, str):
-            logging.debug(f'Got non string {a}; skipping')
+            logging.debug(f"Got non string {a}; skipping")
             continue
 
-        if a == '\x04':  # Check for Ctrl+D (ASCII character '\x04')
-            logging.debug('EOD Ctrl+D')
+        if a == "\x04":  # Check for Ctrl+D (ASCII character '\x04')
+            logging.debug("EOD Ctrl+D")
             break
 
         args.outfile.write(a)
         args.outfile.flush()
-        stdscr.addstr(1, 1, a)
+
+        if args.center:
+            char_y, char_x, _, _ = get_positions(stdscr, True)
+
+        stdscr.addstr(char_y, char_x, a)
         char_count += 1
 
         if char_count % args.timestamp_interval == 0:
             args.outfile.write(ts())
 
-        if char_count % args.click_interval == 0:
+        if char_count % args.click_interval == 0 and not args.quiet:
             playsound(click_sound_path, block=False)
 
         if char_limit and char_count >= char_limit:
@@ -60,21 +79,19 @@ def run(stdscr):
             break
 
 
-
 def main():
     global args
 
-    print('running module rash')
+    print("running module rash")
     parser = ArgumentParser()
-    parser.add_argument(
-        'outfile',
-        type=FileType('a')
-    )
-    parser.add_argument('-l', '--length', type=int, default=None)
-    parser.add_argument('-m', '--minutes', type=int, default=None)
-    parser.add_argument('-c', '--click-interval', type=int, default=50)
-    parser.add_argument('-t', '--timestamp-interval', type=int, default=1000)
-    parser.add_argument('-p', '--prompt', type=str, default=None)
+    parser.add_argument("outfile", type=FileType("a"))
+    parser.add_argument("-l", "--length", type=int, default=None)
+    parser.add_argument("-m", "--minutes", type=int, default=None)
+    parser.add_argument("-c", "--click-interval", type=int, default=50)
+    parser.add_argument("-t", "--timestamp-interval", type=int, default=1000)
+    parser.add_argument("-p", "--prompt", type=str, default=None)
+    parser.add_argument("-C", "--center", action="store_true", default=False)
+    parser.add_argument("-q", "--quiet", action="store_true", default=False)
 
     args = parser.parse_args()
     try:
@@ -82,7 +99,9 @@ def main():
     except KeyboardInterrupt:
         pass
 
-    playsound(end_sound_path, block=False)
+    if not args.quiet:
+        playsound(end_sound_path, block=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
